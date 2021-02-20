0297d5445b9eec6c0a0487732be6e59ee45c7c7e
masterUNG <phrombutr@gmail.com>
2021-02-21 03:59:17 +0700

    update 21Feb

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ungteach/router.dart';
+import 'package:ungteach/utility/my_style.dart';
 
 String initialRoute = '/authen';
 
@@ -21,7 +22,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.brown),
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(color: MyStyle().darkColor),
+        ),
+      ),
       routes: routes,
       initialRoute: initialRoute,
     );
--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,12 +1,16 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ungteach/utility/background.dart';
 import 'package:ungteach/utility/dialog.dart';
 import 'package:ungteach/utility/my_style.dart';
+import 'package:http/http.dart' as http;
 
 class Authen extends StatefulWidget {
   @override
@@ -41,8 +45,10 @@ class _AuthenState extends State<Authen> {
             buildAppName(),
             buildUser(),
             buildPassword(),
-            buildLogin(),
+            buildSignWithEmail(),
             buildSignWithGmail(),
+            buildSignWithFacebook(),
+            buildSignWithAppleId(),
           ],
         ),
       ],
@@ -55,9 +61,75 @@ class _AuthenState extends State<Authen> {
       child: SignInButton(
         Buttons.GoogleDark,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(30),
         ),
         onPressed: () => signWithGmail(),
+      ),
+    );
+  }
+
+  Container buildSignWithFacebook() {
+    return Container(
+      width: 250,
+      child: SignInButton(
+        Buttons.FacebookNew,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        onPressed: () => singWithFacebook(),
+      ),
+    );
+  }
+
+  final FacebookLogin facebookLogin = FacebookLogin();
+
+  Future<Null> singWithFacebook() async {
+    print('SingWithFacebook');
+    final result = await facebookLogin.logIn(['email']);
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final String token = result.accessToken.token;
+        final graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${token}');
+        final profile = json.decode(graphResponse.body);
+        print('######### profile ==> $profile');
+        await Firebase.initializeApp().then((value) async {
+          
+        });
+        break;
+      default:
+    }
+  }
+
+  Container buildSignWithAppleId() {
+    return Container(
+      width: 250,
+      child: SignInButton(
+        Buttons.AppleDark,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Container buildSignWithEmail() {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: 250,
+      child: SignInButton(
+        Buttons.Email,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        onPressed: () {
+          if ((user?.isEmpty ?? true) || (password?.isEmpty ?? true)) {
+            normalDialog(context, 'Have Space ? Please Fill All Blank');
+          } else {
+            checkAuthen();
+          }
+        },
       ),
     );
   }
@@ -83,7 +155,6 @@ class _AuthenState extends State<Authen> {
             Navigator.pushNamed(context, '/myService');
           });
         });
-        
       });
     });
   }
@@ -98,13 +169,7 @@ class _AuthenState extends State<Authen> {
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         ),
-        onPressed: () {
-          if ((user?.isEmpty ?? true) || (password?.isEmpty ?? true)) {
-            normalDialog(context, 'Have Space ? Please Fill All Blank');
-          } else {
-            checkAuthen();
-          }
-        },
+        onPressed: () {},
         child: Text('Login'),
       ),
     );
@@ -125,7 +190,6 @@ class _AuthenState extends State<Authen> {
         keyboardType: TextInputType.emailAddress,
         style: TextStyle(color: MyStyle().darkColor),
         decoration: InputDecoration(
-          hintStyle: TextStyle(color: MyStyle().darkColor),
           prefixIcon: Icon(
             Icons.perm_identity,
             color: MyStyle().darkColor,
@@ -154,10 +218,8 @@ class _AuthenState extends State<Authen> {
               setState(() {
                 statusRedEye = !statusRedEye;
               });
-              print('statusRedEye = $statusRedEye');
             },
           ),
-          labelStyle: TextStyle(color: MyStyle().darkColor),
           prefixIcon: Icon(
             Icons.lock,
             color: MyStyle().darkColor,
